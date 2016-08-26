28f03cf50d38649975cfe5d4222d487902ce7773
mshock <shockleyme@gmail.com>
2016-08-26 01:14:22 +0000

    added run bot server shell cmd

--- golf-bot.js
+++ golf-bot.js
@@ -5,13 +5,23 @@ var config = require('./config');
 var client = new Discord.Client();
 
 client.on("message", function(message) {
-    var chan_gen = client.servers.get("name", "Code Golf").channels.get("name", "general");
+    var default_channel = client.servers.get("name", "Code Golf").defaultChannel;
     var m = message.content
     if(m === "ping") {
         client.reply(message, "pong");
     }
     else if (m === "greeting") { 
-        client.sendMessage(chan_gen, "Hi Chat!");
+        var greetings = ["I think you ought to know I'm feeling very depressed", "This will all end in tears", "Do you want me to sit in a corner and rust or just fall apart where I'm standing?"];
+        var greeting = greetings[Math.floor(Math.random()*greetings.length)];
+        client.sendMessage(default_channel, greeting);
+    }
+    // wacky unsafe PoC, TODO: run in docker container
+    else if (m.startsWith("run ")) { 
+        var exec = require('child_process').exec;
+        var cmd = m.replace(/run /, '');
+        exec(cmd, function(error, stdout, stderr) {
+            client.sendMessage(default_channel, stdout);
+        });
     }
 });
 
